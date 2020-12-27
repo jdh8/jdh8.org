193b7527bcf0c7d9fc1431bc551099c3b179a661
Chen-Pang He <chen.pang.he@jdh8.org>
2020-12-27 15:31:44 +0800

    Update MathJax to 3

--- tools/whiteboard/dom.js
+++ tools/whiteboard/dom.js
@@ -1,9 +1,8 @@
 "use strict";
 {
-	const form = document.forms.board;
 	const display = document.getElementById("display");
 
-	form.addEventListener("submit", function(ev)
+	document.forms.board.addEventListener("submit", function(ev)
 	{
 		ev.preventDefault();
 		ev.stopPropagation();
@@ -11,6 +10,6 @@
 		display.innerHTML = this.elements.html.value;
 
 		if (this.elements.jax.checked)
-			MathJax.Hub.Typeset(display);
+			MathJax.typeset([display]);
 	});
 }
